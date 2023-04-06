ae3b372865f961804a4bad0701ce95970a3f1aea
githubb <binodkumarsahu951@gmail.com>
2023-04-06 11:04:06 +0530

    2 programs

---
+++ averageHeightInanArray.js
@@ -0,0 +1,8 @@
+let arr = [2.2,1,3,1.9,2.4,1.7]
+let length =6
+let includesEle = 0;
+
+for(let i = 0; i<arr.length; i++){
+    includesEle += arr[i]
+}
+console.log(includesEle/length);
---
+++ eachWordFirstLetCaps.js
@@ -0,0 +1,16 @@
+let input = "the quick brown fox";
+// let conArr = input.split(" ")
+// let c = ""
+// for(let i = 0; i < conArr.length; i++){
+//     // c += conArr[i].charAt(0).toUpperCase() + conArr[i].slice(1)+" "
+//     c += conArr[i].charAt(0).toUpperCase() + conArr[i].slice(1) + " "
+//     // let op = val.charAt(0).toUpperCase() + val.slice(1)
+//     // console.log(op);
+// }
+// console.log(c);
+
+let data;
+for(let i =0; i < input.length; i++){
+    
+}
+console.log(data);
